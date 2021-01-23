533861b5703bc766242ba5313f5bd38f535cb1d2
github username <charliepbarth@gmail.com>
2021-01-23 15:46:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(s) {
+    return s.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static titleize(s) {
+    s = s.split(" ")
+    const bad = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+
+    for(let i = 0; i < s.length; i++) {
+      if (i === 0) {
+        s[i] = this.capitalize(s[i])
+      } else {
+        if (!(bad.includes(s[i]))) {
+          s[i] = this.capitalize(s[i])
+        }
+      }
+    }
+
+    return s.join(" ")
+  }
 }
